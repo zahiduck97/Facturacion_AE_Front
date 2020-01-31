abac045f1c0cbdd5fa4d16674844d64ed864af8f
zahiduck97 <zahid.butron@gmail.com>
2020-01-30 20:25:19 -0600

    falta terminar lo del pdf

--- src/app/views/formularios/modals/informacion-servicio/informacion-servicio.component.ts
+++ src/app/views/formularios/modals/informacion-servicio/informacion-servicio.component.ts
@@ -28,14 +28,14 @@ export class InformacionServicioComponent implements OnInit {
   public array_Anexos_dictamen = [];
   public arrayAnexos:string[]=[ 'Etiqueta', 'Empaque o Artes', 'Imágenes', 'Instructivo', 'Garantía' ];
   public arrayAnexosDictamen:string[]=[ 'Pedimento', 'Factura' ];
-  
+
 
   // Constructor
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data:any, 
+    @Inject(MAT_DIALOG_DATA) public data:any,
     public dialogRef: MatDialogRef<InformacionEmpresaComponent>,
-    public dialog: MatDialog, 
-    private _formulario: FormulariosService, 
+    public dialog: MatDialog,
+    private _formulario: FormulariosService,
     private AngularFire: AngularFireStorage,
     ) { }
 
@@ -60,7 +60,7 @@ export class InformacionServicioComponent implements OnInit {
             title: 'Error',
             text: 'El servidor no esta conectado'
           })
-        else 
+        else
           Swal.fire({
             icon: 'error',
             title: 'Error',
@@ -83,7 +83,7 @@ export class InformacionServicioComponent implements OnInit {
       var dictamen_temp = {};
 
       if(formulario_temp.Id_Dictamen){
-        this._formulario.getDictamen(formulario_temp.Id_Dictamen) 
+        this._formulario.getDictamen(formulario_temp.Id_Dictamen)
           .subscribe( async resul => {
             const respuesta = this.dialog.open(EditarFormComponent, {
               data: { form: formulario_temp, dict: resul },
@@ -100,7 +100,7 @@ export class InformacionServicioComponent implements OnInit {
             console.log('hubo pedos');
           })
       } else {
-    
+
         const respuesta = this.dialog.open(EditarFormComponent, {
           data: { form: formulario_temp, dict: dictamen_temp },
           width: '700px'
@@ -141,7 +141,7 @@ export class InformacionServicioComponent implements OnInit {
               'error'
             )
             console.log(e);
-          }) 
+          })
         } else {
           this._formulario.deleteConstancia(this.data.info.Id_Form).subscribe(res => {
             Swal.fire(
@@ -157,7 +157,7 @@ export class InformacionServicioComponent implements OnInit {
               'error'
             )
             console.log(e);
-          }) 
+          })
         }
       }
     });
@@ -171,7 +171,7 @@ export class InformacionServicioComponent implements OnInit {
       var dictamen_temp = {};
 
       if(formulario_temp.Id_Dictamen){
-        this._formulario.getDictamen(formulario_temp.Id_Dictamen) 
+        this._formulario.getDictamen(formulario_temp.Id_Dictamen)
           .subscribe( async resul => {
             const respuesta = this.dialog.open(FacturaComponent, {
               data: { form: formulario_temp, dict: resul },
@@ -188,7 +188,7 @@ export class InformacionServicioComponent implements OnInit {
             console.log('hubo pedos');
           })
       } else {
-    
+
         const respuesta = this.dialog.open(FacturaComponent, {
           data: { form: formulario_temp, dict: dictamen_temp },
           width: '700px'
@@ -222,21 +222,54 @@ export class InformacionServicioComponent implements OnInit {
       img.src = url;
     });
   }
-  
+
   // Final Service
   async generarService(){
-    let perro = 'perro ytfrtygyytf';
-    const data = { 
+    const data = {
       content: [
-        `${perro}` 
+        {
+          margin: [15, 115, 0, 0],
+          table: {
+            widths: [93, 93, 93, 93, 93],
+            body: [
+              [{text: 'Tipo de Servicio:            Dictamen ( )            Constancia ( )              Modificacion ( )', colSpan: 5},
+                {}, {}, {}, {}],
+              [{text: 'No. de Solicitud: 20091USDNOM-004-SCFI-2006000001', colSpan: 3}, {}, {},
+                {text: 'Fecha: 02/01/2020'},
+                {text: 'Elabora: AVC'}],
+              [{text: 'DATOS DEL CLIENTE ', alignment: 'center', colSpan: 5}, {}, {}, {}, {}],
+              [{text: 'Denominación o Razón Social:   LINEA MAGNA S.A. DE C.V', colSpan: 5}, {}, {}, {}, {}],
+              [{text: 'RFC: LMA870811URA', colSpan: 3}, {}, {}, {text: 'No. de Contrato: 18091UCS000169', colSpan: 2}, {}],
+              [{text: 'Domicilio Fiscal'}, {text: 'Calle o Vialidad: JUAN SALVADOR AGRAZ', colSpan: 2}, {},
+                {text: 'No. Ext. 40'}, {text: 'No. Int. PISO 8'}],
+              [{text: 'Colonia: SANTA FE CUAJIMALPA', colSpan: 4}, {}, {}, {}, {text: 'C.P.: 04568'}],
+              [{text: 'Alcaldia o Municipio: CUAJIMALPA DE MORELOS', colSpan: 3}, {}, {}, {text: 'Entidad Federativa: CIUDAD DE MEXICO', colSpan: 2}, {}],
+              [{text: 'Representante Legal: MUSSALI SHAAB SALOMON', colSpan: 3}, {}, {}, {text: 'Tramitador: LAURA JUAREZ CABRERA', colSpan: 2}, {}],
+              [{text: 'Telefono: 55 52924050', colSpan: 3}, {}, {}, {text: 'e-mail: salomon@lineamagna.com', colSpan: 2}, {}],
+              [{text: 'INFORMACION DEL PRODUCTO PARA DICTAMEN / CONSTANCIA', alignment: 'center', colSpan: 5}, {}, {}, {}, {}],
+              [{text: 'Norma en la que se solicita el servicio: NOM-004-SCFI-2006', colSpan: 5}, {}, {}, {}, {}],
+              [{text: 'Denominación del Producto: CHAMARRAS, PANTALON, CAMISAS POLO, T-SHIRT, SUDADERA, PULLOVER PANTALON CORTO, BAÑADORES;', colSpan: 5}, {}, {}, {}, {}],
+              [{text: 'Marca: SEGUN PEDIMENTO', colSpan: 2}, {}, {text: 'Pais de Origen: CHINA, FILIPINAS, INDONESIA, BANGLADESH', colSpan: 3}, {}, {}],
+              [{text: 'Modelo (s): SEGUN PEDIMENTO', colSpan: 5}, {}, {}, {}, {}],
+              [{text: 'Presentación: SEGUN PEDIMENTO', colSpan: 2}, {}, {text: 'Contenido: SEGUN PEDIMENTO', colSpan: 3}, {}, {}],
+              [{text: 'Informacion anexa para Constancia: Etiqueta ( ) Arte ( ) Imagenes ( ) Instructivo ( ) Garantia ( )', colSpan: 5}, {}, {}, {}, {}],
+            ]
+          }
+        },
       ],
+      styles: {
+        bold: {
+          bold: true
+        }
+      }
+      ,
       pageSize: 'LETTER',
       background: [
         {
           image: await this.getBase64ImageFromURL(
-            "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.15752-9/p1080x2048/81947463_445969826309234_7412464939819859968_n.jpg?_nc_cat=101&_nc_ohc=1K2HcCX2QXIAX_7PxBw&_nc_ht=scontent-dfw5-1.xx&_nc_tp=1&oh=53b9460fe42f33fab4270cf979bd1290&oe=5E97C8C3"
+            'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.15752-9/p1080x2048/81947463_445969826309234_7412464939819859968_n.jpg?_nc_cat=101&_nc_ohc=1K2HcCX2QXIAX_7PxBw&_nc_ht=scontent-dfw5-1.xx&_nc_tp=1&oh=53b9460fe42f33fab4270cf979bd1290&oe=5E97C8C3'
           ),
-          width: 792
+          width: 620
         }
       ]
     };
@@ -257,15 +290,15 @@ export class InformacionServicioComponent implements OnInit {
   //         title: 'Error',
   //         text: 'El servidor no esta conectado'
   //       })
-  //     else 
+  //     else
   //       Swal.fire({
   //         icon: 'error',
   //         title: 'Error',
   //         text: e.error.mensaje
   //       })
-      
+
   //     this.preloaderActivo = false;
   //     this.desactivado = false;
-  //   }); 
+  //   });
   // }
 }
